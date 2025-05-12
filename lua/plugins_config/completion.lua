6d71204fdf836c2ba18b88ff34118b05315f8ef4
Adson <adsongabrielfsilva@gmail.com>
2025-05-12 11:05:06 -0300

    feat: changing mapping to only accept suggestion with tab

--- lua/plugins_config/completion.lua
+++ lua/plugins_config/completion.lua
@@ -1,26 +1,32 @@
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		['<Tab>'] = function(fallback)
-			if require('copilot.suggestion').is_visible() then
-				require('copilot.suggestion').accept()
-		      	else
-				fallback()
-		      	end
-		end,
-	}),
-	sources = cmp.config.sources({
-		{name = "copilot"},
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" }
-	}),
-})
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
 
+    ["<CR>"] = cmp.mapping(function(fallback)
+      fallback()
+    end, { "i", "s" }),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      local copilot = require("copilot.suggestion")
+      if copilot.is_visible() then
+        copilot.accept()
+      elseif cmp.visible() then
+        cmp.confirm({ select = true })
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "copilot" },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }),
+})
